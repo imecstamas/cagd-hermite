45b681855d7b18534da466270dff9d14bc00d741
Balazs Erik <balazs.erik.510@gmail.com>
2017-05-27 12:58:30 +0300

    Implement continue to North

--- QtFramework/Hermite/HermiteSurfaces3.cpp
+++ QtFramework/Hermite/HermiteSurfaces3.cpp
@@ -17,7 +17,37 @@ GLvoid ContinueExistingPatch(BicubicHermitePatch3 *patch, PatchDirection dir){
     BicubicHermitePatch3 new_patch;
     switch (dir) {
     case 0:
-        //NORTH
+        DCoordinate3 cord;
+        patch->GetCorner(0, 0, cord);
+        new_patch.SetCorner(1, 0, cord);
+        patch->GetVTangent(0, 0, cord);
+        new_patch.SetVTangent(1, 0, cord);
+        patch->GetUTangent(0, 0, cord);
+        new_patch.SetUTangent(1, 0, cord);
+        patch->GetTwistVector(0, 0, cord);
+        new_patch.SetTwistVector(1, 0, cord);
+
+        patch->GetCorner(0, 1, cord);
+        new_patch.SetCorner(1, 1, cord);
+        patch->GetVTangent(0, 1, cord);
+        new_patch.SetVTangent(1, 1, cord);
+        patch->GetUTangent(0, 1, cord);
+        new_patch.SetUTangent(1, 1, cord);
+        patch->GetTwistVector(0, 1, cord);
+        new_patch.SetTwistVector(1, 1, cord);
+
+        DCoordinate3 newCorner00, oldCorner10;
+        patch->GetCorner(0,0, newCorner00);
+        patch->GetCorner(1,0, oldCorner10);
+        newCorner00 = 2*newCorner00 - oldCorner10;
+        new_patch.SetCorner(0, 0, newCorner00);
+
+        DCoordinate3 newCorner01, oldCorner11;
+        patch->GetCorner(0,1, newCorner01);
+        patch->GetCorner(1,1, oldCorner11);
+        newCorner01 = 2*newCorner01 - oldCorner11;
+        new_patch.SetCorner(0, 1, newCorner01);
+
         break;
     case 1:
         //NORTH-WEST
