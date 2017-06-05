19ccb4d53ae31c1f5b07e61a30377cca2d026054
Balazs Erik <balazs.erik.510@gmail.com>
2017-06-05 22:22:55 +0300

    Implement missing updates

--- QtFramework/Hermite/HermiteSurfaces3.cpp
+++ QtFramework/Hermite/HermiteSurfaces3.cpp
@@ -269,6 +269,16 @@ GLvoid HermiteSurface3::UpdateExistingPatch(BicubicHermitePatch3 *patch, Attribu
         attribute.patch->SetUTangent(0,1, cord);
         patch->GetTwistVector(0,0, cord);
         attribute.patch->SetTwistVector(0,1, cord);
+
+        patch->GetCorner(1,0, cord);
+        attribute.patch->SetCorner(1,1, cord);
+        patch->GetVTangent(1,0, cord);
+        attribute.patch->SetVTangent(1,1, cord);
+        patch->GetUTangent(1,0, cord);
+        attribute.patch->SetUTangent(1,1, cord);
+        patch->GetTwistVector(1,0, cord);
+        attribute.patch->SetTwistVector(1,1, cord);
+
     } else if (dir == S) {
         patch->GetCorner(1,0, cord);
         attribute.patch->SetCorner(0,0, cord);
@@ -323,8 +333,27 @@ GLvoid HermiteSurface3::UpdateExistingPatch(BicubicHermitePatch3 *patch, Attribu
         attribute.patch->SetUTangent(1, 0, cord);
         patch->GetTwistVector(1, 1, cord);
         attribute.patch->SetTwistVector(1, 0, cord);
+    } else if (dir == SE) {
+        patch->GetCorner(1,1, cord);
+        attribute.patch->SetCorner(0,0, cord);
+        patch->GetVTangent(1,1, cord);
+        attribute.patch->SetVTangent(0,0, cord);
+        patch->GetUTangent(1,1, cord);
+        attribute.patch->SetUTangent(0,0, cord);
+        patch->GetTwistVector(1,1, cord);
+        attribute.patch->SetTwistVector(0,0, cord);
+    }  else if (dir == SW) {
+        patch->GetCorner(1,0, cord);
+        attribute.patch->SetCorner(0,1, cord);
+        patch->GetVTangent(1,0, cord);
+        attribute.patch->SetVTangent(0,1, cord);
+        patch->GetUTangent(1,0, cord);
+        attribute.patch->SetUTangent(0,1, cord);
+        patch->GetTwistVector(1,0, cord);
+        attribute.patch->SetTwistVector(0,1, cord);
     }
     attribute.img = attribute.patch->GenerateImage(30,30,GL_STATIC_DRAW);
     attribute.img ->UpdateVertexBufferObjects();
+
 }
 
